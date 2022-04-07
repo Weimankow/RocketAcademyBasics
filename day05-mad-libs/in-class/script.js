78f5d4bcd404a0f903e2155ff30461aad14ffc80
Weimankow <weimankow@gmail.com>
2022-04-07 21:35:59 +0800

    Day 5 - In class - Madlibs exercise

--- day05-mad-libs/in-class/script.js
+++ day05-mad-libs/in-class/script.js
@@ -1,35 +1,88 @@
+// Take input and store in an array  - called list of adjectives
+// When user inputs another word, add to end of array
+// If the input == create
+// get a random adjective from array
+// return sentence with adjective
+
+//Exercise 1
+
+var listOfAdj = [];
+
 var madLibsAdjectivesMain = function (input) {
   // Complete the Base: Mad Libs Adjectives exercise below with madLibsAdjectivesMain as the main function.
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+
+  if (input != "create") {
+    listOfAdj.push(input);
+    console.log(listOfAdj);
+    return `List of Adjectives so far: ${listOfAdj}`;
+  }
+
+  var getRandomIndex = function (arrayLength) {
+    // create a random number from zero to array length minus one.
+    // this number is in the range from the first
+    // index (zero) to the last index (array length minus one)
+    var randomIndex = Math.floor(Math.random() * arrayLength);
+    return randomIndex;
+  };
+
+  if (input == "create") {
+    var randomAdj = listOfAdj[getRandomIndex(listOfAdj.length)];
+    return `"WOW!" he said EXCITEDLY as he jumped into his convertible PAPAYA and drove off with his ${randomAdj} wife.`;
+  }
 };
+
+//Exercise 2
+
+var gameMode = "input";
 
 var madLibsInputCreateMain = function (input) {
   // Complete the Comfortable: Input and Create Mode exercise below with madLibsInputCreateMain as the main function.
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+
+  var getRandomIndex = function (arrayLength) {
+    // create a random number from zero to array length minus one.
+    // this number is in the range from the first
+    // index (zero) to the last index (array length minus one)
+    var randomIndex = Math.floor(Math.random() * arrayLength);
+    return randomIndex;
+  };
+
+  if (input == "create") {
+    gameMode = "madLib";
+  }
+
+  if (gameMode == "madLib") {
+    var randomAdj = listOfAdj[getRandomIndex(listOfAdj.length)];
+    return `"WOW!" he said EXCITEDLY as he jumped into his convertible PAPAYA and drove off with his ${randomAdj} wife.`;
+  }
+  if (input !== "create") {
+    listOfAdj.push(input);
+    console.log(listOfAdj);
+    return `List of Adjectives so far: ${listOfAdj}`;
+  }
 };
+
+//Exercise 3
 
 var madLibsMultipleWordsMain = function (input) {
   // Complete the Comfortable: Input Multiple Words exercise below with madLibsMultipleWordsMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var madLibsMultipleTypesMain = function (input) {
   // Complete the More Comfortable: Mad Libs Multiple Word Types exercise below with madLibsMultipleTypesMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var madLibsPopularMain = function (input) {
   // Complete the More Comfortable: Popular Mad Libs exercise below with madLibsPopularMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var madLibsSetsMain = function (input) {
   // Complete the More Comfortable: Sets of Mad Libs exercise below with madLibsSetsMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
--- day05-mad-libs/pre-class/script.js
+++ day05-mad-libs/pre-class/script.js
@@ -1,11 +1,55 @@
-var followArraysMain = function (input) {
-  // Attempt the Follow Along exercise from the Arrays module below with followArraysMain as the main function.
-  var myOutputValue = 'hello world';
-  return myOutputValue;
-};
+// Store guesses in a global array that persists across Submits.
+// var guesses = [];
+
+// var rollDice = function () {
+//   return Math.floor(Math.random() * 10 + 1);
+// };
+
+// var followArraysMain = function (input) {
+//   // Attempt the Follow Along exercise from the Arrays module below with followArraysMain as the main function.
+//   // Add the user's guess to the guesses array.
+//   guesses.push(input);
+
+//   // // Generate a random dice number.
+//   var randomDiceNumber = rollDice();
+//   // // Initialise output to communicate loss.
+//   // // Output the record of all guesses regardless of loss or win.
+//   console.log(randomDiceNumber);
+//   var myOutputValue = "You lose. Your guesses: " + guesses;
+
+//   // // If the guess matches the dice roll, change output to communicate win.
+//   if (randomDiceNumber == input) {
+//     myOutputValue = "You win. Your guesses: " + guesses;
+//   }
+//   // Return output value.
+//   return myOutputValue;
+// };
+var names = [];
 
 var followArraysLoopsMain = function (input) {
   // Attempt the Follow Along exercise from the Looping Over An Array module below with followArraysLoopsMain as the main function.
-  var myOutputValue = 'hello world';
+
+  // Set a boolean value found to a default value of false
+  var found = false;
+
+  // Loop over the names array, and set found to true if the input name already
+  // exists in the names array
+  var index = 0;
+
+  for (var index = 0; index < names.length; index += 1) {
+    var currentName = names[index];
+    if (currentName == input) {
+      found = true;
+      return `${currentName} already exists. All names in address book: ${names}`;
+    }
+  }
+
+  // If no duplicate name was found, add the input name to the names array
+  if (!found) {
+    names.push(input);
+  }
+
+  // Return the full array of names
+  var myOutputValue = "All names in address book: " + names;
   return myOutputValue;
 };
