76c7218e602f0ec6236064a79b0f8003b3d16d5e
Weimankow <weimankow@gmail.com>
2022-04-08 22:58:18 +0800

    Project 2: Beat it - 1 player can roll and choose their number

--- project2-beat-that/script.js
+++ project2-beat-that/script.js
@@ -4,12 +4,12 @@ There are two players in this game
 The two player takes turns, 
       so when one player has entered an input, the game mode switches to the other player
 When one player clicks submit, the game rolls two dice
-      so there should be a roll dice function that will be called twice
+✅     so there should be a roll dice function that will be called twice
 The output message shows the results of the dice roll
-      so there should be two variables, diceRoll1 and diceRoll2, and we should output the value
+✅      so there should be two variables, diceRoll1 and diceRoll2, and we should output the value
 The player enters whether they want the first dice or second dice to go first.
-      so maybe input '1' will change the player1Choice to 'diceRoll1,Diceroll2', concatinated into a number. 
-      while input '2' will change player1Choice to 'diceRoll2, diceRoll1' 
+✅     so maybe input '1' will change the player1Choice to 'diceRoll1,Diceroll2', concatinated into a number. 
+✅    while input '2' will change player1Choice to 'diceRoll2, diceRoll1' 
 After both players click submit, get the dice number, then choose the dice order, the player with the higher combined number winse
       so computer will have to compare the final choices, if player1Choice is > player2Choice, player 1 wins
 Output who wins
@@ -17,13 +17,38 @@ Output who wins
 */
 
 var playerTurn = "Player1Rolls";
+var player1FinalNumber = "";
+var player1Dice1 = "";
+var player1Dice2 = "";
 
 var rollDice = function () {
   return Math.ceil(Math.random() * 6);
 };
 
 var main = function (input) {
-  var player1Dice1 = rollDice();
-  var player1Dice2 = rollDice();
-  return `${player1Dice1}  ${player1Dice2}`;
+  if (playerTurn === "Player1Rolls") {
+    player1Dice1 = rollDice();
+    player1Dice2 = rollDice();
+    playerTurn = "Player1Chooses";
+    console.log(player1Dice1);
+    console.log(player1Dice2);
+    console.log(playerTurn);
+
+    return `Player 1 rolls! <br>
+   First Dice 🎲: ${player1Dice1} <br>
+   Second Dice 🎲: ${player1Dice2}`;
+  }
+  if (playerTurn === "Player1Chooses") {
+    if (input == "1") {
+      player1FinalNumber = Number(`${player1Dice1}${player1Dice2}`);
+      console.log(player1FinalNumber + 1);
+      return player1FinalNumber;
+    }
+    if (input == "2") {
+      player1FinalNumber = Number(`${player1Dice2}${player1Dice1}`);
+      console.log(player1FinalNumber + 1);
+      playerTurn = "Player2Rolls";
+      return player1FinalNumber;
+    }
+  }
 };
